e36ddeda90d766408f8eac876ffe41840c072a37
d00434525 <d00434525@dmail.dixie.edu>
2022-06-30 09:41:40 -0600

    fixed code

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 /*
-
 // Example quiz object might look like:
-
 var QUIZ = [
     {
         title: "Question 1",
@@ -26,17 +24,75 @@ var QUIZ = [
         userAnswer: ""
     }
 ]
-
 */
+var QUIZ = [
+    {
+        title: "Question 1",
+        questionText: "What is the 3rd letter in the alphabet?",
+        answers: [
+            {text: "a", correct: false},
+            {text: "b", correct: false},
+            {text: "c", correct: true},
+            {text: "d", correct: false}
+        ],
+        userAnswer: ""
+    },
+    {
+        title: "Question 2",
+        questionText: "How many licks to the center of a tootsie pop?",
+        answers: [
+            {text: "3", correct: true},
+            {text: "682", correct: false},
+            {text: "841", correct: false},
+            {text: "1000", correct: false},
+        ],
+        userAnswer: ""
+    },
+    {
+        title: "Question 3",
+        questionText: "How many licks to the center of a tootsie pop?",
+        answers: [
+            {text: "3", correct: true},
+            {text: "682", correct: false},
+            {text: "841", correct: false},
+            {text: "1000", correct: false},
+        ],
+        userAnswer: ""
+    },
+    {
+        title: "Question 4",
+        questionText: "How many licks to the center of a tootsie pop?",
+        answers: [
+            {text: "3", correct: true},
+            {text: "682", correct: false},
+            {text: "841", correct: false},
+            {text: "1000", correct: false},
+        ],
+        userAnswer: ""
+
+    },
+    {
+        title: "Question 5",
+        questionText: "How many licks to the center of a tootsie pop?",
+        answers: [
+            {text: "3", correct: true},
+            {text: "682", correct: false},
+            {text: "841", correct: false},
+            {text: "1000", correct: false},
+        ],
+        userAnswer: ""
+
+    }
+]
 
 
 
 var app = new Vue({
     el: "#app",
     data:{
-        page: 'welcome_page',
         // to attach your global quiz variable to a data variable:
         // myQuiz: QUIZ,
+        myquiz: QUIZ,
     },
     methods:{
         // used for showing questions individually
@@ -45,9 +101,10 @@ var app = new Vue({
         // used for showing questions individually
         previousQuestion : function () {},
 
-        setPage: function (new_page) {
-            this.page = new_page;
+        setPage: function (page) {
+            this.page = page;
         },
+
         
         calculateScore: function () {}
     },
